3e519d43fbf5992626f979b793340a72175ac883
mrhanf15-stack <233031296+mrhanf15-stack@users.noreply.github.com>
2026-04-13 03:25:58 -0400

    fix: Menue exakt wie low_stock_attributes.php Format
    - Sub-Array Key mit Menuename: [BOX_HEADING_TOOLS][$name][]
    - Sprach-Switch per $_SESSION['language_code']
    - Kein MODULE_STATUS Check (Modul hat kein Systemmodul)

--- admin/includes/extra/menu/mrh_product_attributes.php
+++ admin/includes/extra/menu/mrh_product_attributes.php
@@ -1,20 +1,33 @@
 <?php
 /**
- * MRH Product Attributes - Admin Menu Entry
- * Autoinclude: ~/admin/includes/extra/menu/
+ * --------------------------------------------------------------
+ * Modul:       MRH Product Attributes - Admin-Menue
+ * Datei:       admin/includes/extra/menu/mrh_product_attributes.php
+ * Version:     1.0.3
  *
- * Adds a menu entry under "Hilfsprogramme" in the admin navigation.
- *
- * @package MRH_Product_Attributes
- * @version 1.0.2
+ * Autoinclude: Fuegt den Menuepunkt "MRH Produkteigenschaften" im Admin hinzu
+ *              unter der Kategorie "Hilfsprogramme" (BOX_HEADING_TOOLS)
+ * Kompatibilitaet: modified eCommerce v2.0.7.2 rev 14622
+ * --------------------------------------------------------------
  */
 
 defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 
-$add_contents[BOX_HEADING_TOOLS][] = array(
+// Menuename je nach Sprache
+$_mrh_pa_menu_name = 'MRH Produkteigenschaften';
+if (isset($_SESSION['language_code'])) {
+    switch ($_SESSION['language_code']) {
+        case 'en': $_mrh_pa_menu_name = 'MRH Product Attributes'; break;
+        case 'fr': $_mrh_pa_menu_name = 'MRH Attributs Produits'; break;
+        case 'es': $_mrh_pa_menu_name = 'MRH Atributos Productos'; break;
+        default:   $_mrh_pa_menu_name = 'MRH Produkteigenschaften'; break;
+    }
+}
+
+$add_contents[BOX_HEADING_TOOLS][$_mrh_pa_menu_name][] = array(
     'admin_access_name' => 'mrh_product_attributes',
     'filename'          => 'mrh_product_attributes.php',
-    'boxname'           => 'MRH Produkteigenschaften',
+    'boxname'           => $_mrh_pa_menu_name,
     'parameters'        => '',
     'ssl'               => '',
 );
